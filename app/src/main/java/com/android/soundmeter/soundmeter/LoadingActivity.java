79002d9d48eca479ff6bbb149d101af0d6c3d234
norapol <norapol_peem@hotmail.com>
2016-04-17 23:59:51 +0700

    Initial commit

---
+++ app/src/main/java/com/android/soundmeter/soundmeter/LoadingActivity.java
@@ -0,0 +1,31 @@
+package com.android.soundmeter.soundmeter;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import com.android.soundmeter.R;
+
+public class LoadingActivity extends Activity {
+
+    private ProgressDialog mDlg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDlg = ProgressDialog.show(LoadingActivity.this, "", getString(R.string.loading), true, false);
+        startActivity(new Intent(LoadingActivity.this, SoundMeter.class));
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
